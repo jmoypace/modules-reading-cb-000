9d1a814add2289cddf95ecbb678c5378510aff1e
jmoypace <jmoypace@gmail.com>
2018-07-21 02:36:37 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,10 +3,10 @@ require_relative './dance_module.rb'
 
 class Dancer
 
-  
+
   attr_accessor :name
-  include Dance
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   def initialize(name)
     @name = name
   end
